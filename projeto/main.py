42daab82d5f2b58a470035fef6bc90514c9c63f8
Victuhdf7 <victorhugodutraferreira@gmail.com>
2026-07-10 01:54:05 -0300

    feat: adiciona inicio e fim no menu da automacao

--- projeto/main.py
+++ projeto/main.py
@@ -4,5 +4,6 @@ from automacao.automacao_clientes import AutomacaoClientes
 if __name__ == "__main__":
     
     print(f"****MENU INICIAL - AUTOMACAO****")
-    print(f"Iniciando automação de clientes...")
+    print(f"[INFO] Iniciando automacao de clientes...")
     AutomacaoClientes().executar()
+    print(f"[INFO] Automacao concluida!")
